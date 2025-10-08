a05d6e1cb7a4c8dad787d207cc71e8dc7e0b4bcd
sinspired <ggmomo@gmail.com>
2025-10-09 03:40:52 +0800

    feat: 修复版本自动更新的重启逻辑,相应的一些优化

---
+++ assets/sub_run_linux.go
@@ -0,0 +1,15 @@
+//go:build linux
+// +build linux
+
+package assets
+
+import (
+    "os/exec"
+    "syscall"
+)
+
+func setSysProcAttr(cmd *exec.Cmd) {
+    cmd.SysProcAttr = &syscall.SysProcAttr{
+        Setpgid: true,
+    }
+}
--- utils/signal.go
+++ utils/signal.go
@@ -1,31 +1,72 @@
 package utils
 
 import (
-	"fmt"
 	"log/slog"
 	"os"
 	"os/signal"
 	"sync/atomic"
 	"syscall"
+	"time"
 )
 
 // 内部状态跟踪
-// var ctrlCOccurred atomic.Bool
+var ctrlCOccurred atomic.Bool
 
 // SetupSignalHandler 设置信号处理
 // 同时支持两种信号处理模式：
 // - HUB 信号(SIGHUP): 只设置 check.ForceClose 为 true，不退出程序
 // - Ctrl+C 信号(SIGINT/SIGTERM): 第一次设置 ForceClose，第二次退出程序
-func SetupSignalHandler(forceClose *atomic.Bool) {
-	slog.Debug("设置信号处理器")
+// SetupSignalHandler 设置信号处理，返回一个退出信号通道
+func SetupSignalHandler(forceClose *atomic.Bool, checking *atomic.Bool) <-chan struct{} {
+    slog.Debug("设置信号处理器")
 
-	// 监听 SIGINT (Ctrl+C)
-	// ctrlCSigChan := make(chan os.Signal, 1)
-	// signal.Notify(ctrlCSigChan, syscall.SIGINT)
+    stop := make(chan struct{})
 
-	// 监听 SIGHUP (类似 Nginx 的 HUB 信号)
-	hubSigChan := make(chan os.Signal, 1)
-	signal.Notify(hubSigChan, syscall.SIGHUP)
+    ctrlCSigChan := make(chan os.Signal, 1)
+    signal.Notify(ctrlCSigChan, syscall.SIGINT, syscall.SIGTERM)
+
+    hubSigChan := make(chan os.Signal, 1)
+    signal.Notify(hubSigChan, syscall.SIGHUP)
+
+    go func() {
+        for sig := range ctrlCSigChan {
+            slog.Debug("收到中断信号", "sig", sig)
+
+            // 如果正在检测中
+            if checking.Load() {
+                if ctrlCOccurred.CompareAndSwap(false, true) {
+                    forceClose.Store(true)
+                    slog.Warn("已发送停止检测信号，正在等待结果收集。再次按 Ctrl+C 将立即退出程序")
+                    continue
+                }
+            }
+
+            // 否则（空闲状态，或第二次 Ctrl+C）
+            slog.Info("收到退出信号，立即退出程序")
+            select {
+            case <-stop: // 已经关闭过
+            default:
+                close(stop) // 通知 app.Run() 退出
+            }
+
+            // 保险：5s 后强制退出
+            time.AfterFunc(5*time.Second, func() {
+                slog.Error("程序未能正常退出，强制终止")
+                os.Exit(0)
+            })
+        }
+    }()
+
+    go func() {
+        for sig := range hubSigChan {
+            slog.Info("收到 HUB 信号", "sig", sig)
+            forceClose.Store(true)
+            slog.Info("HUB 模式: 已设置强制关闭标志，任务将自动结束，程序继续运行")
+        }
+    }()
+
+    return stop
+}
 
 	// // 处理 Ctrl+C 信号
 	// // 会导致子进程也接收到信号，所以不要这个了。
@@ -49,15 +90,3 @@ func SetupSignalHandler(forceClose *atomic.Bool) {
 	// 		}
 	// 	}
 	// }()
-
-	// 处理 HUB 信号
-	go func() {
-		for sig := range hubSigChan {
-			slog.Debug(fmt.Sprintf("收到 HUB 信号: %s", sig))
-
-			// HUB 信号只设置 ForceClose，不退出程序
-			forceClose.Store(true)
-			slog.Debug("HUB 模式: 已设置强制关闭标志，任务将自动结束，程序继续运行")
-		}
-	}()
-}
