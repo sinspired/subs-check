abe915dc76ebf244fb60d95dab0767ee2cb3888b
sinspired <ggmomo@gmail.com>
2025-11-16 03:54:03 +0800

    perf: 使用HEAD方法测活

--- check/platform/google.go
+++ check/platform/google.go
@@ -1,14 +1,13 @@
 package platform
 
 import (
-	"io"
 	"log/slog"
 	"net/http"
 )
 
 func CheckGoogle(httpClient *http.Client) (bool, error) {
 	if success, err := checkGoogleEndpoint(httpClient, "https://gstatic.com/generate_204", 204); err == nil && success {
-		return checkGoogleEndpoint(httpClient, "https://www.google.com/generate_204", 204) 
+		return checkGoogleEndpoint(httpClient, "https://www.google.com/generate_204", 204)
 	}
 	return false, nil
 }
@@ -20,25 +19,24 @@ func CheckGstatic(httpClient *http.Client) (bool, error) {
 	return false, nil
 }
 
+// checkGoogleEndpoint 使用 HEAD 方法检查 URL 的状态码。
 func checkGoogleEndpoint(httpClient *http.Client, url string, statusCode int) (bool, error) {
-	req, err := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest("HEAD", url, nil)
 	if err != nil {
 		return false, err
 	}
 
-	// 添加请求头,模拟正常浏览器访问
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
 	req.Header.Set("Accept-Language", "en-US,en;q=0.5")
 	req.Header.Set("Connection", "close")
 
-	// 发送请求
 	resp, err := httpClient.Do(req)
 	if err != nil {
 		slog.Debug(err.Error())
 		return false, err
 	}
 	defer resp.Body.Close()
-	io.Copy(io.Discard, resp.Body) // 确保读完
+
 	return resp.StatusCode == statusCode, nil
 }
